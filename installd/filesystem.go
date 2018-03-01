7fce0bd6acaf0e76b2b89f5f6bae25471e3c4dd4
Till Wegmüller <toasterson@gmail.com>
2018-03-01 11:33:46 +0100

    Fixed: Update to new uuid library Fixed: Mistakenly commited file FIxed: use correct diskname of test vm Added: sample_data to ignore

--- installd/filesystem.go
+++ installd/filesystem.go
@@ -87,7 +87,7 @@ func createDatasets(conf *InstallConfiguration, noop bool) error {
 			glog.Infof("Creating BootEnvironment %s/ROOT/%s", conf.Rpool, conf.BEName)
 			var bootenv *zfs.Dataset
 			if bootenv, err = zfs.CreateDataset(fmt.Sprintf("%s/ROOT/%s", conf.Rpool, conf.BEName), zfs.DatasetTypeFilesystem, map[string]string{"mountpoint": altRootLocation}, true); err == nil {
-				u1 := uuid.NewV4()
+				u1, _ := uuid.NewV4()
 				bootenv.SetProperty("org.opensolaris.libbe:uuid", u1.String())
 				glog.Infof("Success")
 			} else {
